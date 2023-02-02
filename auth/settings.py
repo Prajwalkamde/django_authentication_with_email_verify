cbac4b045101dd197653a0b12e1cc99f84206ad4
Prajwal <prajwalkamde264@gmail.com>
2023-02-02 18:30:20 +0530

    added build files

--- auth/settings.py
+++ auth/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-efoz^gy!9n#1otnr%1i8p@%mk0o(m#4&afjv6b0y5mw3n5^sxq'
+SECRET_KEY = {'django-insecure-efoz^gy!9n#1otnr%1i8p@%mk0o(m#4&afjv6b0y5mw3n5^sxq'}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
