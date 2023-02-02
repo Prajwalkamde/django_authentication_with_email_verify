f92060e95984ba20125e7dc1fafad7810f96d96b
Prajwal <prajwalkamde264@gmail.com>
2023-02-02 18:09:46 +0530

    added build files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-echo " BUILD START"
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
-# echo " BUILD END"
+# echo " BUILD START"
+# pip install -r requirements.txt
+# python3.9 manage.py collectstatic
+# # echo " BUILD END"
