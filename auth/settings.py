9bc11131636ad4ce2b67ccec41b0ad03bb0e5ec2
Prajwal <prajwalkamde264@gmail.com>
2023-02-03 20:58:40 +0530

    secured data

--- auth/settings.py
+++ auth/settings.py
@@ -133,8 +133,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'perfectboy0987@gmail.com'
-EMAIL_HOST_PASSWORD = 'dxcfytfxejldfekn'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 
